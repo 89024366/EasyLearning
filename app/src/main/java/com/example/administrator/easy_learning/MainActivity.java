3f41d18defdaaa8d835ce4a5f0d99ee10ca2a259
ZY <1400190395@qq.com>
2018-03-27 01:26:52 +0800

    prototype

---
+++ app/src/main/java/com/example/administrator/easy_learning/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.administrator.easy_learning;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private Detail f_detail;
+    private Notebook f_notebook;
+    private Search f_search;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    //mTextMessage.setText(R.string.title_home);
+                    showNav(R.id.navigation_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                   // mTextMessage.setText(R.string.title_dashboard);
+                    showNav(R.id.navigation_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    //mTextMessage.setText(R.string.title_notifications);
+                    showNav(R.id.navigation_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void init(){
+        f_detail=new Detail();
+        f_notebook=new Notebook();
+        f_search=new Search();
+        FragmentTransaction beginTransaction=getFragmentManager().beginTransaction();
+        beginTransaction.add(R.id.content,f_search).add(R.id.content,f_detail).add(R.id.content,f_notebook);//开启一个事务将fragment动态加载到组件
+       // beginTransaction.hide(f_search);
+        //beginTransaction.hide(f_detail);
+        //beginTransaction.hide(f_notebook);//隐藏fragment
+        //beginTransaction.replace(R.id.content,f_search);
+        beginTransaction.addToBackStack(null);//返回到上一个显示的fragment
+        beginTransaction.commit();//每一个事务最后操作必须是commit（），否则看不见效果
+        showNav(R.id.navigation_home);
+    }
+
+    private void showNav(int navid){
+        FragmentTransaction beginTransaction=getFragmentManager().beginTransaction();
+
+        switch (navid){
+            case R.id.navigation_home:
+                beginTransaction.hide(f_detail).hide(f_notebook).show(f_search);
+                //beginTransaction.replace(R.id.content,f_search);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                break;
+            case R.id.navigation_dashboard:
+                beginTransaction.hide(f_search).hide(f_notebook).show(f_detail);
+                //beginTransaction.replace(R.id.content,f_detail);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                break;
+            case R.id.navigation_notifications:
+                beginTransaction.hide(f_detail).hide(f_search).show(f_notebook);
+                //beginTransaction.replace(R.id.content,f_notebook);
+               beginTransaction.addToBackStack(null);
+               beginTransaction.commit();
+                break;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/administrator/easy_learning/Search.java
@@ -0,0 +1,20 @@
+package com.example.administrator.easy_learning;
+
+/**
+ * Created by Administrator on 2018/3/26.
+ */
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.example.administrator.easy_learning.R;
+
+public class Search extends Fragment{
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.search,container,false);
+        return view;
+    }
+}
